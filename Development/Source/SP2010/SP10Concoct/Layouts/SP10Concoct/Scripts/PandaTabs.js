1f25adec7ad9cbae2d129b58a624997bd27f2df8
sagarpandey88_cp <SND\sagarpandey88_cp>
2014-06-14 07:45:40 +0000

    Initial Checkin 1. Jscripts 2. SAP Connect 3. Caml Helper 4. Concoct Framework

---
+++ Development/Source/SP2010/SP10Concoct/Layouts/SP10Concoct/Scripts/PandaTabs.js
@@ -0,0 +1,23 @@
+﻿/****************************************/
+/*			PANDA TABS					*/
+/****************************************/
+
+//Load Time Functions
+$(document).ready(function () {
+    //Hide all the tabs at load time
+    $('div[isTab=true]').toggle();
+
+    //Attach on click listner to all the anchor elements with isTabSwitch=true attribute
+    $('a[isTabSwitch=true]').click(function () {
+        ToggleTab(this);
+
+    });
+});
+
+
+
+
+function ToggleTab(tabSwitch) {
+    $('div[isTab=true]').hide(500);
+    $('#' + tabSwitch.value).toggle(500);
+}
