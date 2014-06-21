5141b963f88ebb17829cf9e3f03ad8119f782771
sagarpandey88_cp <30863296+sagarpandey88@users.noreply.github.com>
2014-06-21 06:12:30 +0000

    Updated Common Project 1. Encryption Code 2. Common Class with handy methods 3. SPCommon which includes regularly used SP Scenario based methods

---
+++ Development/Source/SP2010/SPConcoct.Common/Common.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SPConcoct.Common
+{
+    public class Common
+    {
+
+        private static string RemoveFBAStringLoginName(string loginName)
+        {
+            string FBAKey = System.Configuration.ConfigurationManager.AppSettings["MembershipProvider"];
+
+            if (loginName.Contains(FBAKey))
+            {
+                loginName = loginName.Replace(FBAKey, "");
+            }
+
+            return loginName;
+        }
+
+
+        public static List<string> GetCommaSeperatedItems(string commaSeperatedValues)
+        {
+            List<string> seperatedValue = new List<string>();
+            if (!string.IsNullOrEmpty(commaSeperatedValues))
+            {
+                commaSeperatedValues = commaSeperatedValues.TrimEnd(',');
+            }
+
+            if (commaSeperatedValues.Contains(","))
+            {
+                seperatedValue = commaSeperatedValues.Split(',').ToList();
+            }
+
+            return seperatedValue;
+
+        }
+
+
+
+       
+    }
+}
---
+++ Development/Source/SP2010/SPConcoct.Common/Encryption.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SPConcoct.Common
+{
+    class Encryption
+    {
+
+        #region Local variables
+
+        //Define default password length.        
+        //private static int DEFAULT_PASSWORD_LENGTH = 8;
+        private const string ENCRYPTION_KEY = "key";
+        private const string PARAMETER_NAME = "enc=";
+
+        #endregion
+        #region Encryption/decryption
+
+        /// <summary>
+        /// The salt value used to strengthen the encryption.
+        /// </summary>
+        private readonly static byte[] SALT = Encoding.ASCII.GetBytes(ENCRYPTION_KEY.Length.ToString());
+
+        /// <summary>
+        /// Encrypts any string using the Rijndael algorithm.
+        /// </summary>
+        /// <param name="inputText">The string to encrypt.</param>
+        /// <returns>A Base64 encrypted string.</returns>
+        public static string Encrypt(string inputText)
+        {
+            RijndaelManaged rijndaelCipher = new RijndaelManaged();
+
+
+            byte[] plainText = Encoding.Unicode.GetBytes(inputText);
+
+            PasswordDeriveBytes SecretKey = new PasswordDeriveBytes(ENCRYPTION_KEY, SALT);
+
+            using (ICryptoTransform encryptor = rijndaelCipher.CreateEncryptor(SecretKey.GetBytes(32), SecretKey.GetBytes(16)))
+            {
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
+                    {
+                        cryptoStream.Write(plainText, 0, plainText.Length);
+                        cryptoStream.FlushFinalBlock();
+                        return Convert.ToBase64String(memoryStream.ToArray());
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Decrypts a previously encrypted string.
+        /// </summary>
+        /// <param name="inputText">The encrypted string to decrypt.</param>
+        /// <returns>A decrypted string.</returns>
+        public static string Decrypt(string inputText)
+        {
+            RijndaelManaged rijndaelCipher = new RijndaelManaged();
+
+            byte[] encryptedData = Convert.FromBase64String(inputText.Replace(' ', '+'));
+            PasswordDeriveBytes secretKey = new PasswordDeriveBytes(ENCRYPTION_KEY, SALT);
+
+            using (ICryptoTransform decryptor = rijndaelCipher.CreateDecryptor(secretKey.GetBytes(32), secretKey.GetBytes(16)))
+            {
+                using (MemoryStream memoryStream = new MemoryStream(encryptedData))
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read))
+                    {
+                        byte[] plainText = new byte[encryptedData.Length];
+                        int decryptedCount = cryptoStream.Read(plainText, 0, plainText.Length);
+                        return Encoding.Unicode.GetString(plainText, 0, decryptedCount);
+                    }
+                }
+            }
+        }
+
+        #endregion
+
+
+    }
+}
