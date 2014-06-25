a67b5660211fde8b8b8fa73ac56b26e97ef07254
sagarpandey88_cp <SND\sagarpandey88_cp>
2014-06-25 10:29:14 +0000

    Added CustomChartMethods and Scrollable GV js and JSON Helper Class

---
+++ Development/Source/SP2010/SP10Concoct/Layouts/SP10Concoct/Scripts/CustomChartMethod.js
@@ -0,0 +1,64 @@
+﻿
+function CreateChart(url, dvId, title, legendText, chartType) {
+
+    $(document).ready(function () {
+
+        $.ajax(
+            {
+                type: "POST",
+                url: url,
+                data: "",
+                dataType: 'json',
+                success: function (msg) {
+                    var data = msg;
+                    // alert(data);
+                    initChart(data, dvId, title, legendText, chartType);
+                }
+            });
+    });
+}
+
+
+function initChart(data, dvId, title, legendText, chartType) {
+
+    var background = {
+        type: 'linearGradient',
+        x0: 0,
+        y0: 0,
+        x1: 0,
+        y1: 1,
+        colorStops: [{ offset: 0, color: '#d2e6c9' },
+                             { offset: 1, color: 'white'}]
+    };
+
+    $('#' + dvId).jqChart({
+        title: { text: title },
+        animation: { duration: 1 },
+        legend: { title: legendText },
+        border: { strokeStyle: '#6ba851' },
+        background: background,
+        series: [
+                            {
+                                type: chartType,
+                                labels: {
+                                    // stringFormat: '%.1f%%',
+                                    // valueType: 'percentage',
+                                    font: '15px sans-serif',
+                                    fillStyle: 'white'
+                                },
+                                data: data
+                            }
+                        ]
+    });
+
+
+    $('#' + dvId).bind('tooltipFormat', function (e, data) {
+        var percentage = data.series.getPercentage(data.value);
+        percentage = data.chart.stringFormat(percentage, '%.2f%%');
+
+        return '<b>' + data.dataItem[0] + '</b></br>' +
+                       data.value + ' (' + percentage + ')';
+    });
+
+
+}
